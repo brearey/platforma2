49be2b4226ef52b9c05a2baca7a4312994824530
Евгений <brearey4@gmail.com>
2024-05-09 22:32:41 +0900

    Try use namespaces

---
+++ api/TaskController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Api;
+
+use RedBeanPHP;
+
+session_start();
+require_once($GLOBALS["root_path"] . "utils/autoload.php");
+require_once(dirname(__FILE__) . getPath('/../utils/autoload.php'));
+require_once(dirname(__FILE__) . getPath('/../utils/functions.php'));
+
+// if (isset($_GET["delete"])) {
+//     if (isset($_GET["id"])) {
+//         $id = $_GET["id"];
+
+//         $task = R::load("Task", $id);
+
+//         if (!$task) {
+//             $_SESSION["errors"] = "task not exists";
+//         } else {
+//             R::trash($task);
+//             $_SESSION["success"] = "data deleted succesfully";
+//         }
+
+
+
+
+//         // redirection 
+//         header("location:../index.php");
+//     }
+// }
+
+class TaskController
+{
+    private $table = "tasks";
+    public function deleteTask($id)
+    {
+        $task = R::load($this->table, $id);
+        errorHandler($task, "task not exists");
+        $trash = R::trash($task);
+        errorHandler($trash, "trash error");
+        successHandler("task deleted");
+        header("location:../index.php");
+    }
+
+    public function addTask($name)
+    {
+        $task = R::dispense($this->table);
+        errorHandler($task, "error dispense task");
+        $task->name = $name;
+        $task->is_done = false;
+        $id = R::store($task);
+        errorHandler($id, "error add task");
+        successHandler("task created");
+        header("location:../index.php");
+    }
+
+    public function updateTask($id, $name, $is_done)
+    {
+        $task = R::load($this->table, $id);
+        errorHandler($task, "error load task");
+        $task->name = $name;
+        $task->is_done = $is_done;
+        $id = R::store($task);
+        errorHandler($id, "error update task");
+        successHandler("task updated");
+        header("location:../index.php");
+    }
+}
